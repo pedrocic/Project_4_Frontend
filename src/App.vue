3aa072fe7dc334794ae0f54395261da1bba7b0ff
pedrocic <pedro.unb.cic@gmail.com>
2020-04-27 10:52:28 -0300

    Escopo do frontend

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <Header></Header>
+    <Menu></Menu>
+    <Content></Content>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/templates/Header";
+import Menu from "@/components/templates/Menu";
+import Content from "@/components/templates/Content";
+import Footer from "@/components/templates/Footer";
+
+export default {
+  name: "App",
+  components: { Header, Menu, Content, Footer }
+};
+</script>
+
+<style>
+* {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0;
+  padding: 0;
+}
+
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+  height: 100vh;
+
+  display: grid;
+  grid-template-rows: 60px 1fr 40px;
+  grid-template-columns: 300px 1fr;
+  grid-template-areas:
+    "header header"
+    "menu content"
+    "menu footer";
+}
+</style>
