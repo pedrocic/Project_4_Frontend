44082357e327cf0e25912dd9cbfb3d086d7059a3
pedrocic <pedro.unb.cic@gmail.com>
2020-04-27 15:11:59 -0300

    Add Routes

---
+++ src/components/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <PageTitle icon="fa fa-home" main="Dashboard" sub="Base de dados" />
+  </div>
+</template>
+
+<script>
+import PageTitle from "../templates/PageTitle";
+export default {
+  name: "Home",
+  components: { PageTitle }
+};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import Vue from "vue";
 import App from "./App.vue";
 
 import store from "./config/store";
+import router from "./config/routes";
 import "./config/bootstrap";
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount("#app");
