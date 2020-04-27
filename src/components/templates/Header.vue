461f2530b9f3a076e3694541ac7df867032db430
pedrocic <pedro.unb.cic@gmail.com>
2020-04-27 11:47:36 -0300

    Menu com MapState

--- src/components/templates/Header.vue
+++ src/components/templates/Header.vue
@@ -23,7 +23,7 @@ export default {
   },
   methods: {
     toggleMenu() {
-      console.log("ToggleMenu");
+      this.$store.commit("toggleMenu");
     }
   }
 };
--- src/config/store.js
+++ src/config/store.js
@@ -14,8 +14,6 @@ export default new Vuex.Store({
       } else {
         state.isMenuVisible = isVisible;
       }
-
-      console.log("toggleMenu =" + state.isMenuVisible);
     }
   }
 });
